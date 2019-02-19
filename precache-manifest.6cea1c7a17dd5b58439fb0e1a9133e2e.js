10efcb2e18ed91a57550a2ba81e673980a8ed6c0
Valeriy <highwaytowork@gmail.com>
2019-02-19 23:05:13 +0500

    Updates

---
+++ precache-manifest.6cea1c7a17dd5b58439fb0e1a9133e2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e722ce830d329033e0e",
+    "url": "/Vabak/Memory-game/static/css/main.c8048924.chunk.css"
+  },
+  {
+    "revision": "4e722ce830d329033e0e",
+    "url": "/Vabak/Memory-game/static/js/main.4e722ce8.chunk.js"
+  },
+  {
+    "revision": "3598e9fbaa6883f3e5e0",
+    "url": "/Vabak/Memory-game/static/js/1.3598e9fb.chunk.js"
+  },
+  {
+    "revision": "fad43950cbb182e79711",
+    "url": "/Vabak/Memory-game/static/js/runtime~main.fad43950.js"
+  },
+  {
+    "revision": "fa6b4a54a5db922a7938cbe94fdd8a4c",
+    "url": "/Vabak/Memory-game/static/media/StartGame@2x.fa6b4a54.png"
+  },
+  {
+    "revision": "976f641a603cd1e09b86ee70384bcae9",
+    "url": "/Vabak/Memory-game/static/media/Group 2@2x.976f641a.png"
+  },
+  {
+    "revision": "d97bc3b940b57c61ddbd242c934c303b",
+    "url": "/Vabak/Memory-game/index.html"
+  }
+];
