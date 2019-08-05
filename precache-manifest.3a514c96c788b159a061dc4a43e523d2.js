64199e75af9f723c73ba61943c01394832d0304a
Valeriy <highwaytowork@gmail.com>
2019-08-05 17:29:19 +0500

    Updates

--- precache-manifest.3a514c96c788b159a061dc4a43e523d2.js
+++ precache-manifest.3a514c96c788b159a061dc4a43e523d2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "42ac2f6824bc13e33e52",
+    "revision": "c5d57613c9e4276c7de7",
     "url": "/Memory-game/static/css/main.c8048924.chunk.css"
   },
   {
-    "revision": "42ac2f6824bc13e33e52",
-    "url": "/Memory-game/static/js/main.42ac2f68.chunk.js"
+    "revision": "c5d57613c9e4276c7de7",
+    "url": "/Memory-game/static/js/main.c5d57613.chunk.js"
   },
   {
     "revision": "3598e9fbaa6883f3e5e0",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/Memory-game/static/media/Group 2@2x.976f641a.png"
   },
   {
-    "revision": "3566a549b3f2e874cbc16d51f4715532",
+    "revision": "9df176c96155f3b945a624ba84320bd8",
     "url": "/Memory-game/index.html"
   }
 ];
